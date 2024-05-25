b4d31680725dd0c1c80d69f3d808ac47fa0904ab
jyothsna <jessysree11@gmail.com>
2024-05-25 11:01:58 +0200

    Rest API code

--- src/test/java/RestAPIInspired.java
+++ src/test/java/RestAPIInspired.java
@@ -1,37 +1,30 @@
-import com.google.common.graph.EndpointPair;
-import org.testng.annotations.Test;
 
+import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-
-import static io.restassured.RestAssured.authentication;
-import static io.restassured.RestAssured.given;
-
-import org.junit.Assert;
-import io.restassured.path.json.JsonPath;
+import static io.restassured.RestAssured.*;
 
 public class RestAPIInspired {
 
     @Test
     public void jsonIterateArr() {
-        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.baseURI = "https://api.coindesk.com/v1/bpi/currentprice.json";
         Response response = RestAssured.given()
                 .when()
 
-                .get("/api/users/2");
+                .get(baseURI);
 
         if (response.getStatusCode() == 200) {
-            System.out.println("status code:" + response.getStatusCode());
-            String firstName = response.jsonPath().getString("data.first_name");
-            System.out.println("First Name:" + firstName);
-            String url = response.jsonPath().getString("url");
-            System.out.println(RestAssured.baseURI + ("/api/users/2"));
+            System.out.println("status code: " + response.getStatusCode());
+            String rate = response.jsonPath().getString("bpi.USD.rate");
+            System.out.println("USD.RATE: " + rate);
+            String description = response.jsonPath().getString("bpi.USD.description");
+            System.out.println("USD.DESCRIPTION: " + description);
+            System.out.println("URL: " + RestAssured.baseURI);
 
         } else {
             System.out.println("Failed to get data:" + response.getStatusCode());
         }
-
-
     }
 }
 	
